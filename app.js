d7028fc2efe035ad896edcc961bdde8548c11b11
Garza0 <konovalovandrey7@gmail.com>
2020-01-15 22:40:18 +0200

    cookies

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 const storeBtn = document.getElementById('store-btn')
 const retrBtn = document.getElementById('retrieve-btn')
 
-const userId = 'u123'
-const user = {
-  name: 'max',
-  age: 35,
-  hobbies: ['football', 'killing']
-}
-
 storeBtn.addEventListener('click', () => {
-  sessionStorage.setItem('uid', userId)
-  localStorage.setItem('user', JSON.stringify(user))
-
+  const userId = 'u123'
+  const user = { name: 'max', age: 47, hobby: ['start', 'end'] }
+  document.cookie = 'uid=' + userId
+  document.cookie = `user = ${JSON.stringify(user)}`
 })
 
 retrBtn.addEventListener('click', () => {
-  const extractedId = sessionStorage.getItem('uid')
-  const extractedUser = JSON.parse(localStorage.getItem('user'))
-  console.log(extractedUser)
-  if (extractedId) {
-    console.log('Got the id - ' + extractedId)
-  } else {
-    console.log('Could not find any')
-  }
-}
-)
+  console.log(document.cookie)
+})
+
+
