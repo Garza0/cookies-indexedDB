79777358b1bd27023ddd9ee049519f4bda426d87
Garza0 <konovalovandrey7@gmail.com>
2020-01-15 22:10:33 +0200

    session storage

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const user = {
 }
 
 storeBtn.addEventListener('click', () => {
-  localStorage.setItem('uid', userId)
+  sessionStorage.setItem('uid', userId)
   localStorage.setItem('user', JSON.stringify(user))
 
 })
 
 retrBtn.addEventListener('click', () => {
-  const extractedId = localStorage.getItem('uid')
+  const extractedId = sessionStorage.getItem('uid')
   const extractedUser = JSON.parse(localStorage.getItem('user'))
   console.log(extractedUser)
   if (extractedId) {
